ee40d8d583424c9b2e3db36f8f7c47b62356a890
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 01:36:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,10 @@ def summon_captain_planet(elements)
 end
 
 def long_planeteer_calls(calls)
-  calls.
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
